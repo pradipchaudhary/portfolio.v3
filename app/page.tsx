b13d1a1a927b1bf2208bc4ecb374c19c682525f6
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-08-28 08:27:15 +0000

    fixed issu

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import ProjectCard from "@/components/projects/ProjectCard";
 import AboutMe from "@/components/sections/AboutMe";
-
 
 export default function HomePage() {
     return (
         <>
             <AboutMe />
-            <ProjectCard/>
         </>
     );
 }
--- components/ui/Background.tsx
+++ components/ui/Background.tsx
@@ -1,19 +1,3 @@
-// import React from "react";
-
-// const Background = () => {
-//     return (
-//         <div className="fixed inset-0 overflow-hidden">
-//             {/* Base Gradient for a deep black background */}
-//             <div className="fixed inset-0 bg-gradient-to-b from-[#000000] via-[#050505] to-[#101010]" />
-
-//             {/* Vignette Effect - helps add depth and focus to the center */}
-//             <div className="fixed inset-0 bg-[radial-gradient(circle_at_center,transparent_50%,rgba(0,0,0,0.7)_100%)] pointer-events-none" />
-//         </div>
-//     );
-// };
-
-// export default Background;
-
 
 import React from "react";
 
