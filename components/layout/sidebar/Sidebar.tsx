88da2c5f246a31b4261d2d3cacbe5192da5345cd
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-01-17 23:31:12 +0545

    change social icon

--- components/layout/sidebar/Sidebar.tsx
+++ components/layout/sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Check, Github, Linkedin, Facebook, Instagram } from "lucide-react";
+import { Check, Github, Linkedin, Facebook, Instagram, Youtube } from "lucide-react";
 
 
 
@@ -111,8 +111,8 @@ const Sidebar = () => {
           <Link href="https://linkedin.com/in/pradipchaudhary" target="_blank" className="text-gray-500 hover:text-gray-900">
             <Linkedin className="w-5 h-5" />
           </Link>
-          <Link href="https://www.facebook.com/itspradipchaudhary/" target="_blank" className="text-gray-500 hover:text-gray-900">
-            <Facebook className="w-5 h-5" />
+          <Link href="https://www.youtube.com/@pradipchaudhary_codes" target="_blank" className="text-gray-500 hover:text-gray-900">
+            <Youtube className="w-5 h-5" />
           </Link>
           <Link href="https://www.instagram.com/itspradipchaudhary/" target="_blank" className="text-gray-500 hover:text-gray-900">
             <Instagram className="w-5 h-5" />
